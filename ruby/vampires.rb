eb8877631349f0564d8b9461198458155777e27a
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-06 19:10:09 -0700

    Create initial question and answer sequence for vampire detection

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+your_name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birthyear = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+health_ins = gets.chomp
