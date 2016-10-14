a9c1ae8ccd24c7d035a2c43b48c90c009c87dd65
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-13 21:04:27 -0700

    Created data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+continents = {
+	'antartica' => {
+		'cities' => [],
+		'animals' => ['penguins', 'polar bears', 'whales'],
+		'cold?' => true
+	}
+	'north_america' => {
+		'cities' => ['Mexico City', 'New York City', 'Toronto'],
+		'animals' => ['deer', 'seagulls', 'alligators'],
+		'countries' => 23
+	}
+	'asia' => {
+		'cities' => ['Bangkok', 'Tokyo', 'Shanghai'],
+		'animals' => ['Pandas', 'Koi', 'Elephants'],
+		'countries' => 48
+	}
+	'planet' => 'Earth',
+	'galaxy' => 'Milky Way',
+	'how_many' => 7,
+	'not_listed' => ['South America', 'Africa', 'Europe', 'Australia']
+	'edible' => false
+}
+
+sw_movies = [
+	'a_new_hope' => {
+		'release' => 1977,
+		'protagonist' => 'Luke Skywalker',
+		'episode' => 4
+	},
+	'the_empire_strikes_back' => {
+		'release' => 1980,
+		'villain' => 'Darth Vader',
+		'episode' => 5
+	},
+	'return_of_the_jedi' => {
+		'release' => 1983,
+		'evil_hutt' => "Jabba",
+		'episode' =>6
+	},
+	['The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith'],
+	['The Clone Wars Animated', ['The Clone Wars Movie', 'The Clone Wars TV Show']],
+	'The Force Awakens'
+]
