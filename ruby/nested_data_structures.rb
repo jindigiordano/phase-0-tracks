64e6fefca91bf591a2d94b22535f38d0e2719546
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-13 21:22:27 -0700

    Access calls to deeply nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,38 +1,38 @@
 continents = {
-	'antartica' => {
+	'antarctica' => {
 		'cities' => [],
 		'animals' => ['penguins', 'polar bears', 'whales'],
 		'cold?' => true
-	}
+	},
 	'north_america' => {
 		'cities' => ['Mexico City', 'New York City', 'Toronto'],
 		'animals' => ['deer', 'seagulls', 'alligators'],
 		'countries' => 23
-	}
+	},
 	'asia' => {
 		'cities' => ['Bangkok', 'Tokyo', 'Shanghai'],
 		'animals' => ['Pandas', 'Koi', 'Elephants'],
 		'countries' => 48
-	}
+	},
 	'planet' => 'Earth',
 	'galaxy' => 'Milky Way',
 	'how_many' => 7,
-	'not_listed' => ['South America', 'Africa', 'Europe', 'Australia']
+	'not_listed' => ['South America', 'Africa', 'Europe', 'Australia'],
 	'edible' => false
 }
 
 sw_movies = [
-	'a_new_hope' => {
+	{   'title' => 'a_new_hope', 
 		'release' => 1977,
 		'protagonist' => 'Luke Skywalker',
 		'episode' => 4
 	},
-	'the_empire_strikes_back' => {
+	{	'title' => 'the_empire_strikes_back',
 		'release' => 1980,
 		'villain' => 'Darth Vader',
 		'episode' => 5
 	},
-	'return_of_the_jedi' => {
+	{	'title' => 'return_of_the_jedi',
 		'release' => 1983,
 		'evil_hutt' => "Jabba",
 		'episode' =>6
@@ -41,3 +41,17 @@ sw_movies = [
 	['The Clone Wars Animated', ['The Clone Wars Movie', 'The Clone Wars TV Show']],
 	'The Force Awakens'
 ]
+
+p continents['antarctica']['animals'][0]
+
+p continents['north_america']['cities']
+
+p continents['asia']['countries']
+
+p continents['not_listed'][1]
+
+p sw_movies[0]['release']
+p sw_movies[1]['villain']
+p sw_movies[2]['episode']
+p sw_movies[3]
+p sw_movies[4][1][1]
