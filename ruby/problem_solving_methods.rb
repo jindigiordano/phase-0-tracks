fa25052e422514977c4691dd8a9dea2987fda453
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-14 10:43:42 -0700

    Added Fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -20,5 +20,23 @@ end
 arr = [42, 89, 23, 1]
 
 # test calls
-p search_array(arr, 1)
-p search_array(arr, 24)
+# p search_array(arr, 1)
+# p search_array(arr, 24)
+
+
+# Define method to output num of Fibonacci numbers given
+def fib(num)
+	# Set up the first two nums in fib array
+	fib_arr = [0, 1]
+	# To add more nums in array, add the prev two
+	# Add on num - 2 more items in array (first two already given)
+	(num - 2).times { |i|
+		# Add prev two numbers, push sum into array
+		new_num = fib_arr[-1] + fib_arr[-2]
+		fib_arr << new_num
+	}
+	fib_arr
+end
+
+# p fib(6)
+# p fib(100)
