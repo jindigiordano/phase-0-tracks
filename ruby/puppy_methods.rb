b653bc9f05b097ae7146ae4226c9c9c2d17164cc
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-18 12:40:55 -0700

    Added kitten code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,7 +5,59 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times { puts "Woof!" }
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	7 * human_years
+  end
+
+  def run(miles)
+  	puts "I ran #{miles} miles!"
+  end
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
 end
 
+
+
 pup = Puppy.new
-pup.fetch
+pup.fetch("ball")
+pup.speak(3)
+pup.roll_over
+p pup.dog_years(1)
+pup.run(2)
+
+class Cat
+	
+	def initialize
+		puts "Initialzing new cat instance..."
+	end
+	
+	def meow(num)
+		num.times {puts "Meow"}
+	end
+	
+	def drink
+		puts "*drink water*"
+	end
+end
+
+kitten = Cat.new
+kitten.meow(4)
+kitten.drink
+kitten_array = []
+ 50.times {
+ kitten = Cat.new
+ kitten_array << kitten }
+ 
+kitten_array.each {|kitten| kitten.meow(2)
+	kitten.drink}
