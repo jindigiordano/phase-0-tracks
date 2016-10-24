0ab8be1a2b342717e1e935c8a45e27d920ad65e5
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-23 21:27:46 -0700

    Updated word guessing game spec so it passes for: checking if a letter is in word, updating and outputting guesses left, feedback on current state

--- ruby/my_game/game.rb
+++ ruby/my_game/game.rb
@@ -1,13 +1,15 @@
 # Create game class
 class Game
-	attr_reader :win_word, :remaining_guesses, :past_guesses, :current_state
+	attr_reader :win_word, :past_guesses, :hint
+	attr_accessor :remaining_guesses
 
 	# User 1 enters word to initialize
 	def initialize(win_word)
 	# Set winning word
 		@win_word = win_word
-	# Initialize remaining guesses to win_word length
-		@remaining_guesses = win_word.length
+		@win_arr = win_word.split("")
+	# Initialize remaining guesses to win_word length plus 5
+		@remaining_guesses = win_word.length + 5
 	# Initialize array to store past guesses to check for repeats
 		@past_guesses = []
 	# Initialize string to represent current state of word, or, hint
@@ -15,10 +17,8 @@ class Game
 		hint_length = win_word.length
 		hint_length.times { hint = hint + "_ " }
 		@hint = hint
-	end
-
-	def update_hint(letter)
-
+		p "Guess the word!"
+		p "Here's a hint: #{@hint}"
 	end
 
 	def win
@@ -29,17 +29,63 @@ class Game
 		p "Haha, you couldn't guess it! The word was '#{@win_word}'"
 	end
 
+	def update_hint(letter)
+		char_ind = 0
+		@win_word.each_char { |char|
+			if char == letter
+				hint_ind = char_ind * 2
+				@hint[hint_ind] = char
+			end
+			char_ind += 1
+		}
+		hint_str = @hint.delete " "
+		#if the hint is filled up, they win
+		if hint_str == @win_word
+			win
+		#else update the status
+		else
+			p @hint
+		end
+	end
+
+	def guesses_remaining
+			# decrease by one
+			@remaining_guesses -= 1
+	end
+
 	# User 2 guesses letter in word, or a word
 	def guess(guess)
-		if guess == @win_word
+		guess
+		# Make sure the guess is either a letter or the whole word
+		if guess.length != 1 && guess.length != @win_word.length
+			p "Guess a letter, or the complete word!"
+		# check if they guessed the entire word correctly
+		elsif guess == @win_word
 			win
+		# if the letter is not in the word
+		elsif !@win_word.include? guess
+			if @remaining_guesses == 1
+				lose
+			else
+			guesses_remaining
+			p @hint
+			p "Sorry, try again!"
+			end
+		# if the letter is in the word
+		elsif @win_word.include? guess
+			guesses_remaining
+			update_hint(guess)
+			p "Nice guess!"
 		else
-			lose
+			p "Error"
 		end
-
 	end
 
 end
 
-p game = Game.new("robot")
-p game.guess("robot")
+game = Game.new("robot")
+game.guess("p")
+game.guess("o")
+game.guess("r")
+game.guess("b")
+game.guess("t")
