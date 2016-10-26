205cd8a33e56dbb864a53211ddbce55d1d98c8a8
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-25 17:59:18 -0700

    Refactored methods and driver code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -7,6 +7,7 @@
 # Require_relative looks for a file within its same directory that you are using for the given file. #
 # Require takes a longer path, require is also used to pull in modules. #
 require_relative 'state_data'
+require 'pry'
 
 class VirusPredictor
 # Makes a new state instance. #
@@ -17,47 +18,46 @@ class VirusPredictor
   end
 # Consolidates the two methods and has them evaluate virus effects #
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
 # Number of deaths is based on population density, outputs number of deaths for the population. #
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      death_rate = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      death_rate = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      death_rate = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      death_rate = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      death_rate = 0.05
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{(@population * death_rate).floor} people in this outbreak"
 
   end
 
 # Calculates how much time in months it will take for disease to spread across the state #
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -72,17 +72,22 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+STATE_DATA.each { |state_name, pop_hash|
+  #binding.pry
+  VirusPredictor.new(state_name, pop_hash[:population_density], pop_hash[:population]).virus_effects
+}
 
 
 #=======================================================================
