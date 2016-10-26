129547f9ba45a42c92af8919d1846f4bd0b58ec2
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-10-26 16:15:29 -0700

    Updated with random test data, function to generate array

--- js/algos.js
+++ js/algos.js
@@ -11,9 +11,9 @@ function longestPhrase(arrayPhrases) {
 	return longest;
 }
 
-console.log(longestPhrase(["long phrase", "longest phrase", "longer phrase"]));
-console.log(longestPhrase(["jim", "bob", "humphrey"]));
-console.log(longestPhrase(["do or do not", "nooooo", "may the force be with you"]));
+//console.log(longestPhrase(["long phrase", "longest phrase", "longer phrase"]));
+//console.log(longestPhrase(["jim", "bob", "humphrey"]));
+//console.log(longestPhrase(["do or do not", "nooooo", "may the force be with you"]));
 
 
 // function takes 2 obj and checks if they share key-value pairs
@@ -31,5 +31,41 @@ function keyValMatch(obj1, obj2) {
 	return false;
 }
 
-console.log(keyValMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
-console.log(keyValMatch({name: "Anakin", age: 24}, {name: "Obi Wan", age: 34}));
+//console.log(keyValMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+//console.log(keyValMatch({name: "Anakin", age: 24}, {name: "Obi Wan", age: 34}));
+
+//function that takes int for length, builds and returns arr of str of length
+function createWordArr(length) {
+	//array of words to pick from
+	var randomWords = ["a", "be", "the", "yoda", "blast", "bantha", "podrace", "cryonics", "skywalker", "lightsaber"];
+	//initialize array of words to push to and return
+	var finalArray = [];
+	//iterate over the first given number of array items and push to final array
+	for ( i = 0; i < length; i++ ) {
+		//set random word length
+		var wordLength = Math.floor(Math.random() * 10) + 1;
+		//search random word array for word of the random length
+		for ( j = 0; j < randomWords.length; j++ ) {
+			if (randomWords[j].length == wordLength) {
+				//push word to final array
+				finalArray.push(randomWords[j]);
+			}
+		}
+	}
+	return finalArray;
+}
+//console.log(createWordArr(3));
+//console.log(createWordArr(6));
+
+//10 times
+//generate & print array
+//feed arr to longest word fn and prints
+
+
+times = 0;
+while ( times <= 9) {
+	var createdArray = createWordArr(Math.floor(Math.random() * 9) + 1);
+	console.log(createdArray);
+	console.log(longestPhrase(createdArray));
+	times++;
+}
