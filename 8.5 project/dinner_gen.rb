4ac112189b681ec135236aff21f142ae714d5cd0
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-11-05 11:01:16 -0700

    Pseudo coded user interface, added methods for adding items to databse, method for creating tables

--- 8.5 project/dinner_gen.rb
+++ 8.5 project/dinner_gen.rb
@@ -55,33 +55,60 @@ end
 
 #test driver code
 
-add_entree(db, "Honey BBQ Ribs", Faker::Name.name, false)
+add_entree(db, "Honey BBQ Ribs", Faker::Name.name, "false")
 
 # add veggie fn
+def add_entree(db, name, submitted_by, favorite)
+	db.execute("INSERT INTO entree (name, submitted_by, favorite) values (?, ?, ?)", [name, submitted_by, favorite])
+end
+
+add_veggie(db, "Bacon Green Beans", Faker::Name.name, "false")
 
 # add fruit fn
-
-# add dishes to dish table
-# db.execute("INSERT INTO dish (name, dish_type) values ('Honey BBQ Ribs', 'entree')")
-
-# add meals to meal table
-# db.execute("INSERT INTO meal (name, entree_id, side_id, dessert_id) values ('Murica', 1, 2, 3)")
-
-# add dinners
-# db.execute("INSERT INTO dinner (date, host, num_ppl, meal_id) values (?, ?, ?, ?)")
-
-# create lots of dinners
-
-def create_dinner(db, days_ago, invite, num_ppl, meal_id)
-	db.execute("INSERT INTO dinner (day_of_week, invite, num_ppl, meal_id) values (?, ?, ?, ?)", [days_ago, invite, num_ppl, meal_id])
+def add_entree(db, name, submitted_by, favorite)
+	db.execute("INSERT INTO entree (name, submitted_by, favorite) values (?, ?, ?)", [name, submitted_by, favorite])
 end
+
+add_fruit(db, "White Nectarine", Faker::Name.name, "false")
+
+
+# def create_dinner(db, days_ago, invite, num_ppl, meal_id)
+#	db.execute("INSERT INTO dinner (day_of_week, invite, num_ppl, meal_id) values (?, ?, ?, ?)", [days_ago, invite, num_ppl, meal_id])
+# end
 
 
 # explore ORM by retreving data
-dinner = db.execute("SELECT * FROM dinner")
-p dinner;
+# dinner = db.execute("SELECT * FROM dinner")
+# p dinner;
 # dinner.each do |dinner|
 # 	puts "On day #{dinner['day_of_week']} of this week, I suggest cooking #{dinner['meal_id']} for #{dinner['num_ppl']} people and inviting #{dinner['invite']}."
 # end
 
 
+# Prompt user if they'd like to add a dish or be recommended a meal
+puts "Welcome to the dinner generator."
+puts "Select one of the following options:"
+puts "1 - Generate a random dinner idea for me"
+puts "2 - View all dishes"
+puts "3 - Add a dish to the database"
+puts "4 - Add a dish to favorites"
+puts "5 - exit"
+
+# Generate random dinner
+# Randomly select from eeach table using rand and id
+
+# View all dishes
+# Iterate and print all items in each table
+
+# Add a dish to database
+# prompt for what kind of dish
+# add to appropriate table
+
+# Add a dish to favorites
+# Ask what type of dish
+# Print all and ask for id of which to add to fav
+# chage fav value to true
+# print all fav dishes
+
+#exit
+# farewell message
