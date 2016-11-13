d03e2b6c54c7011e8beaed0b11a64442f5725a95
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-11-13 11:46:20 -0800

    Add campuses table and web app

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,16 +1,23 @@
 # require gems
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
+	
 
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
+end
+
+get '/table' do
+	  @students = db.execute("SELECT * FROM students")
+	erb :table
 end
 
 get '/students/new' do
@@ -25,3 +32,46 @@ post '/students' do
 end
 
 # add static resources
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS campuses(
+    id INTEGER PRIMARY KEY,
+    campus VARCHAR(255)
+	)
+	SQL
+  db.execute(create_table_cmd)
+
+	# add campus fn
+	def add_campus(db, campus)
+		db.execute("INSERT INTO campuses (campus) values (?)", [campus])
+	end
+
+  campuses = db.execute("SELECT * FROM campuses")
+	# if campuses table is empty, put some initial values in it
+	if campuses == []
+		add_campus(db, "SF")
+		add_campus(db, "SD")
+		add_campus(db, "NYC")
+		add_campus(db, "CHI")
+		add_campus(db, "SEA")	
+	end
+
+
+# add new campuses
+get '/campus/new' do
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :new_campus
+end
+
+# method for creating new campus
+post '/campuses' do
+
+  db.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+  redirect '/campus_list'
+end
+
+# campus list
+get '/campus_list' do
+	@campuses = db.execute("SELECT * FROM campuses")
+	erb :campus_list
+end
